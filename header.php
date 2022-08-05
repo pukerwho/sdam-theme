948d324305c9b17c21970433c068e191ad98e0e9
Puker Who <nawwesele@gmail.com>
2022-08-05 15:30:08 +0300

    edit

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico"/>
 	<?php wp_head(); ?>
 	<?php echo carbon_get_theme_option('crb_google_analytics'); ?>
 </head>
