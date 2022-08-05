362615a6a9d7a3642f998595bd31a93810a34a53
Puker Who <nawwesele@gmail.com>
2022-08-05 15:56:47 +0300

    edit

--- index.php
+++ index.php
@@ -6,17 +6,33 @@
     <div class="w-full xl:w-2/3 xl:px-10">
       <div class="flex items-center -mx-2 mb-8">
         <div class="px-2">
-          <div class="bg-gray-200 hover:bg-yellow-100 dark:bg-gray-600 hover:dark:bg-gray-700 rounded cursor-pointer px-4 py-2"><?php _e("Нові", "treba-wp"); ?></div>
+          <div class="tab-btn btn-primary cursor-pointer px-4 py-2" data-tab="new"><?php _e("Нові", "treba-wp"); ?></div>
         </div>
         <div class="px-2">
-          <div class="bg-gray-200 hover:bg-yellow-100 dark:bg-gray-600 hover:dark:bg-gray-700 rounded cursor-pointer px-4 py-2"><?php _e("Популярні", "treba-wp"); ?></div>
+          <div class="tab-btn btn-secondary cursor-pointer px-4 py-2" data-tab="popular"><?php _e("Популярні", "treba-wp"); ?></div>
         </div>
       </div>
-      <div>
+      <div class="tab-content" data-tab="new">
         <?php 
           $new_posts = new WP_Query( array( 
             'post_type' => 'places', 
             'posts_per_page' => 10,
+          ) );
+          if ($new_posts->have_posts()) : while ($new_posts->have_posts()) : $new_posts->the_post(); 
+        ?>
+          <div class="mb-6">
+            <?php get_template_part('template-parts/place-item'); ?>
+          </div>
+        <?php endwhile; endif; wp_reset_postdata(); ?>
+      </div>
+      <div class="tab-content hidden" data-tab="popular">
+        <?php 
+          $new_posts = new WP_Query( array( 
+            'post_type' => 'places', 
+            'posts_per_page' => 10,
+            'meta_key' => 'post_count',
+            'orderby' => 'meta_value_num',
+            'order' => 'DESC'
           ) );
           if ($new_posts->have_posts()) : while ($new_posts->have_posts()) : $new_posts->the_post(); 
         ?>
--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,27 +1,29 @@
-let tabsBtn = document.querySelectorAll('.tab-btn');
-let tabsContent = document.querySelectorAll('.tab-content');
+let tabsBtn = document.querySelectorAll(".tab-btn");
+let tabsContent = document.querySelectorAll(".tab-content");
 
 function tabRemove() {
   tabsContent.forEach((item) => {
-    item.classList.add('hidden');
-  })
+    item.classList.add("hidden");
+  });
   tabsBtn.forEach((tab) => {
-    tab.classList.remove('bg-primary');
-    tab.classList.add('bg-main-gray');
-  })
+    tab.classList.remove("btn-primary");
+    tab.classList.add("btn-secondary");
+  });
 }
 
 function tabAdd(tabContent, item) {
-  tabContent.classList.remove('hidden');
-  item.classList.add('bg-primary')
-  item.classList.remove('bg-main-gray');
+  tabContent.classList.remove("hidden");
+  item.classList.add("btn-primary");
+  item.classList.remove("btn-secondary");
 }
 
 tabsBtn.forEach((item) => {
-  item.addEventListener('click', () => {
+  item.addEventListener("click", () => {
     tabData = item.dataset.tab;
-    let tabContent = document.querySelector('.tab-content[data-tab="' + tabData + '"]');
+    let tabContent = document.querySelector(
+      '.tab-content[data-tab="' + tabData + '"]'
+    );
     tabRemove();
     tabAdd(tabContent, item);
-  })
-})
+  });
+});
