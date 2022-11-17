27a7731077a54edb2880bfd04c289cf635c405db
Puker Who <nawwesele@gmail.com>
2022-11-17 23:20:10 +0200

    Edit

--- template-parts/sidebar.php
+++ template-parts/sidebar.php
@@ -54,55 +54,6 @@
   </div>
 <?php endif; ?>
 
-<div class="bg-gray-100 dark:bg-gray-700 shadow-lg rounded border-t-4 border-t-indigo-500 p-4 mb-12">
-  <div class="text-xl uppercase font-bold mb-4"><?php _e("Популярні пропозиції", "treba-wp"); ?></div>
-  <div>
-    <?php 
-      $popular_places = new WP_Query( array( 
-        'post_type' => 'places', 
-        'posts_per_page' => 5,
-        'meta_key' => 'post_count',
-        'orderby' => 'meta_value_num',
-      ) );
-      if ($popular_places->have_posts()) : while ($popular_places->have_posts()) : $popular_places->the_post(); 
-    ?>
-      <div class="relative flex items-center mb-6 last:mb-0">
-        <a href="<?php the_permalink(); ?>" class="absolute-link"></a>
-        <div class="mr-4">
-          <img src="<?php echo get_the_post_thumbnail_url(); ?>" loading="lazy" class="w-[40px] min-w-[60px] h-[40px] min-h-[60px] object-cover rounded-lg"> 
-        </div>
-        <div>
-          <div class="mb-1"><?php the_title(); ?></div>
-          <div class="text-sm opacity-75"><?php _e("Переглядів", "treba-wp"); ?>: <?php echo get_post_meta( get_the_ID(), 'post_count', true ); ?></div>
-        </div>
-      </div>
-    <?php endwhile; endif; wp_reset_postdata(); ?>
-  </div>
-</div>
-
-<div class="bg-gray-100 dark:bg-gray-700 shadow-lg rounded border-t-4 border-t-indigo-500 p-4 mb-12">
-  <div class="text-xl uppercase font-bold mb-4"><?php _e("Нові записи", "treba-wp"); ?></div>
-  <div>
-    <?php 
-      $new_posts = new WP_Query( array( 
-        'post_type' => 'post', 
-        'posts_per_page' => 5,
-      ) );
-      if ($new_posts->have_posts()) : while ($new_posts->have_posts()) : $new_posts->the_post(); 
-    ?>
-      <div class="relative flex items-center mb-6 last:mb-0">
-        <a href="<?php the_permalink(); ?>" class="absolute-link"></a>
-        <div class="mr-4">
-          <img src="<?php echo get_the_post_thumbnail_url(); ?>" loading="lazy" class="w-[40px] min-w-[60px] h-[40px] min-h-[60px] object-cover rounded-lg"> 
-        </div>
-        <div>
-          <div class="mb-1"><?php the_title(); ?></div>
-          <div class="text-sm opacity-75"><?php _e("Переглядів", "treba-wp"); ?>: <?php echo get_post_meta( get_the_ID(), 'post_count', true ); ?></div>
-        </div>
-      </div>
-    <?php endwhile; endif; wp_reset_postdata(); ?>
-  </div>
-</div>
 
 <div class="bg-gray-100 dark:bg-gray-700 shadow-lg  rounded border-t-4 border-t-indigo-500 p-4 mb-12">
   <div class="text-xl uppercase font-bold mb-4"><?php _e("Міста", "treba-wp"); ?></div>
