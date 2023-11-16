e52bdc805b1520b61f20cba3010e87698750dcd9
Puker Who <nawwesele@gmail.com>
2023-11-16 12:03:31 +0200

    edit

---
+++ inc/custom-fields/gutenberg-blocks/important-block.php
@@ -0,0 +1,20 @@
+<?php
+
+use Carbon_Fields\Block;
+use Carbon_Fields\Field;
+
+Block::make( 'TREBA Important Block' )
+	->add_fields( array(
+		Field::make( 'textarea', 'crb_post_important_block', 'Текст' ),
+	) )
+	->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
+		?>
+    
+    <div class="bg-indigo-100 dark:bg-gray-700 border-4 border-indigo-500 dark:border-indigo-500 rounded px-4 pl-4 lg:pl-8 py-4 mb-4">
+      <div class="content">
+        <p><?php echo esc_html( $fields['crb_post_important_block'] ); ?></p>
+      </div>
+    </div>
+
+		<?php
+	} );
